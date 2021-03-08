ad3c1c49a1d9cf2d897a7fce299c50d4b2a43028
Le Thanh Hung <45506820+ahhung77@users.noreply.github.com>
2021-03-08 16:36:09 +0700

    Update ebs_summary.py

--- pscore/mme/ericsson/ebs_summary.py
+++ pscore/mme/ericsson/ebs_summary.py
@@ -4,10 +4,11 @@ from pandas import DataFrame, Series
 import matplotlib.pyplot as plt
 
 ebs_file = 'mmee2b.csv'
-ebs_load = pd.read_csv(ebs_file)[['event_id', 'event_result', 'apn', 'cause_code', 'imsi', 'sub_cause_code']]
+ebs_load = pd.read_csv(ebs_file, nrows=1000000)
+ebs_load = ebs_load[['event_id', 'event_result', 'apn', 'cause_code', 'imsi', 'sub_cause_code']]
 ebs_load = ebs_load[ebs_load['event_id'] != 'event_id']
 
 ebs_load_success = ebs_load[ebs_load['event_result'] == "success"]
 ebs_load_fail = ebs_load[ebs_load['event_result'] != "success"]
 
-mmee2b_ebs[mmee2b_ebs['event_id']=='activate'].groupby('sub_cause_code').count().sort_values(by=['event_id'], ascending=False)['event_id']
+ebs_load_fail.groupby('sub_cause_code').count().sort_values('event_id', ascending=False)
