8ab0743335d8aedb46204eddaa407df2f83897b9
Le Thanh Hung <45506820+ahhung77@users.noreply.github.com>
2021-03-09 15:15:03 +0700

    Update ebs_summary.py

--- pscore/mme/ericsson/ebs_summary.py
+++ pscore/mme/ericsson/ebs_summary.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 ebs_file = 'mmee2b.csv'
 ebs_load = pd.read_csv(ebs_file, nrows=1000000)
-ebs_load = ebs_load[['event_id', 'event_result', 'apn', 'cause_code', 'imsi', 'sub_cause_code']]
+ebs_load = ebs_load[['event_id', 'event_result', 'cause_code','sub_cause_code', 'imsi', 'apn']]
 ebs_load = ebs_load[ebs_load['event_id'] != 'event_id']
 
 ebs_load_success = ebs_load[ebs_load['event_result'] == "success"]
